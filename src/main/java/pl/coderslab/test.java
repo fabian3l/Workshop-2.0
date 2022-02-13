0e0fb43a46d0c81f52b3d52e8af27d8fac5bff34
fabian3l <94535567+fabian3l@users.noreply.github.com>
2022-02-13 12:10:03 +0100

    caly kodzik (nie koniecznie działający)

---
+++ src/main/java/pl/coderslab/test.java
@@ -0,0 +1,14 @@
+package pl.coderslab;
+
+public class test {
+    public static void main (String[] args){
+
+        String[][] arr1 = {
+                {"ania", "kasia", "asia"},
+                {"basia", "asia", "kasia"}
+        };
+
+        System.out.println(arr1.length);
+
+    }
+}
